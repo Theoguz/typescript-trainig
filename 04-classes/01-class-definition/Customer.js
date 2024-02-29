cc47d795f2c65413497730d5ce862bf91d1d35fb
Oguzz <oguzaslan_203@hotmail.com>
2024-02-29 23:32:39 +0300

    Added constructor

--- 04-classes/01-class-definition/Customer.js
+++ 04-classes/01-class-definition/Customer.js
@@ -1,10 +1,14 @@
 var Customer = /** @class */ (function () {
-    function Customer() {
+    function Customer(theFirst, theLast) {
+        this.firstName = theFirst;
+        this.lastName = theLast;
     }
     return Customer;
 }());
-var myCustomer = new Customer();
+var myCustomer = new Customer("Oguz", "Aslan");
+/*
 myCustomer.firstName = "Oguz";
 myCustomer.lastName = "Aslan";
+*/
 console.log(myCustomer.firstName);
 console.log(myCustomer.lastName);
--- 04-classes/01-class-definition/Customer.ts
+++ 04-classes/01-class-definition/Customer.ts
@@ -3,12 +3,19 @@ class Customer {
     firstName: string;
     lastName: string;
 
+    constructor(theFirst: string, theLast: string) {
+        this.firstName = theFirst;
+        this.lastName = theLast;
+
+    }
+
+
 }
 
-let myCustomer = new Customer();
-
+let myCustomer = new Customer( "Oguz", "Aslan");
+/*
 myCustomer.firstName = "Oguz";
 myCustomer.lastName = "Aslan";
-
+*/
 console.log(myCustomer.firstName);
 console.log(myCustomer.lastName);

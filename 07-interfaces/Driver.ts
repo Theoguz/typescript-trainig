d15b35800d18704e20a81c0876385c157546da99
Oguzz <oguzaslan_203@hotmail.com>
2024-03-01 19:42:17 +0300

    Interface added

---
+++ 07-interfaces/Driver.ts
@@ -0,0 +1,15 @@
+import { CricketCoach } from './CricketCoach';
+import { GolfCoach } from './GolfCoach';
+
+let myCricketCoach = new CricketCoach();
+let myGolfCoach = new GolfCoach();
+
+
+let theCoaches: CricketCoach[] = [];
+
+theCoaches.push(myCricketCoach);
+theCoaches.push(myGolfCoach);
+
+for (let tempCoach of theCoaches) {
+    console.log(tempCoach.getDailyWorkout());
+}
